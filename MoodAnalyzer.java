00da0f229a56926937c0b546e03187df7126ac40
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-09 22:37:54 +0530

    Handle Exception if User Provides Invalid Mood

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -1,7 +1,5 @@
 package com.blz.moodanalyzer;
 
-import com.bl.moodanalyzer.MoodAnalysisException.ExceptionType;
-
 public class MoodAnalyzer {	
 	private String message;
 	
@@ -12,18 +10,14 @@ public class MoodAnalyzer {
 		this.message = message;
 	}
 
-	public String analyzeMood() throws MoodAnalysisException {
+	public String analyzeMood() {
 		try {
-			if(message.length() == 0) {
-				throw new MoodAnalysisException(ExceptionType.ENTERED_EMPTY, "You entered an Invalid input!\\nPlease enter a non-empty input for mood.");
-			}
-				
 			if(message.contains("sad"))
 				return "SAD";
 			else
 				return "HAPPY";
 		} catch (NullPointerException e) {
-			throw new MoodAnalysisException(ExceptionType.ENTERED_NULL, "You entered an Invalid input!\\nPlease enter a non-NULL input for mood.");
+			return "HAPPY";
 		}
 	}	
 }
