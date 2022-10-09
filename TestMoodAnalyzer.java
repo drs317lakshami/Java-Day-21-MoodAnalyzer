710007c8eb41f3fe0b32e1403840b477646dcfc9
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-10-09 22:13:53 +0530

    Inform user if entered Invalid Mood

---
+++ TestMoodAnalyzer.java
@@ -0,0 +1,43 @@
+package com.blz.moodanalyzer;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestMoodAnalyzer {
+
+	@Test
+	public void givenMessage_whenSad_shouldReturnSad() throws MoodAnalysisException {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a sad message");
+		String mood = moodAnalyzer.analyzeMood();
+		assertEquals("SAD", mood);
+	}
+	
+	@Test
+	public void givenMessage_whenNotSad_shouldReturnHappy() throws MoodAnalysisException {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a happy message");
+		String mood = moodAnalyzer.analyzeMood();
+		assertEquals("HAPPY", mood);
+	}
+	
+	@Test
+	public void givenMessage_whenNull_shouldThrowException() throws MoodAnalysisException {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+		String mood;
+		try {
+			mood = moodAnalyzer.analyzeMood();
+		} catch (MoodAnalysisException e) {
+			assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL, e.type);
+		}
+	}
+	
+	@Test
+	public void givenMessage_whenEmpty_shouldThrowException() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer(" ");
+		String mood = null;
+		try {
+			mood = moodAnalyzer.analyzeMood();
+		} catch (MoodAnalysisException e) {
+			assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY, e.type);
+		}	
+	}
+}
